9989d578e23c9cba82d38263f665001cb9774605
Yaseer Jawhar <yaseer.jawhar@vetpartners.co.uk>
2023-05-04 22:12:49 +0100

    Authenticate with hardcode

---
+++ cmd/api/handlers.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func (app *application) Home(w http.ResponseWriter, r *http.Request) {
+	var payload = struct {
+		Status  string `json:"status"`
+		Message string `json:"message"`
+		Version string `json:"version"`
+	}{
+		Status:  "active",
+		Message: "Go Movies up and running",
+		Version: "1.0.0",
+	}
+
+	_ = app.writeJSON(w, http.StatusOK, payload)
+}
+
+func (app *application) AllMovies(w http.ResponseWriter, r *http.Request) {
+	movies, err := app.DB.AllMovies()
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	_ = app.writeJSON(w, http.StatusOK, movies)
+}
+
+func (app *application) authenticate(w http.ResponseWriter, r *http.Request) {
+	//read json payload
+
+	//validate user against database
+
+	//check password
+
+	//create a jwt user
+	u := jwtUser{
+		ID:        1,
+		FirstName: "Admin",
+		LastName:  "User",
+	}
+
+	//generate tokens
+	tokens, err := app.auth.GenerateTokenPair(&u)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	log.Println(tokens.Token)
+
+	w.Write([]byte(tokens.Token))
+}
